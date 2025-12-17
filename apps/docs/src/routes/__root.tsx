35be8af3018f7823c49bd2882698df9516797d72
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-12-17 08:29:51 -0600

    docs: update metadata for seo WIP

--- apps/docs/src/routes/__root.tsx
+++ apps/docs/src/routes/__root.tsx
@@ -6,12 +6,33 @@ import {
 } from "@tanstack/react-router";
 import { RootProvider } from "fumadocs-ui/provider/tanstack";
 
+import { generateMetadata } from "@/lib/util/generateMetadata";
 import appCss from "../../styles/app.css?url";
 
 export const Route = createRootRoute({
-  head: () => ({
-    links: [{ rel: "stylesheet", href: appCss }],
-  }),
+  head: () => {
+    const { meta, links } = generateMetadata({
+      description: "Generate TanStack artifacts that just fit.",
+      icons: [
+        { rel: "shortcut icon", url: "/img/favicon.ico" },
+        { rel: "icon", url: "/img/favicon-32x32.png" },
+        { rel: "apple-touch-icon", url: "/img/apple-touch-icon.png" },
+      ],
+      manifest: "/img/site.webmanifest",
+      twitter: {
+        card: "summary_large_image",
+        creator: "@hobbescodes",
+        images: "/tangrams-logo.png",
+        title: "Tangrams",
+        description: "Generate TanStack artifacts that just fit.",
+      },
+    });
+
+    return {
+      meta,
+      links: [...links, { rel: "stylesheet", href: appCss }],
+    };
+  },
   component: RootComponent,
 });
 
